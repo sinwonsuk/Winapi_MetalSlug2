998b2f8867ac06fa7582712674ca01773c9df9a0
LJS0113 <krescent2912@gmail.com>
2023-02-17 17:47:35 +0900

    포트폴리오

---
+++ GameEngineContents/HeavyGun.cpp
@@ -0,0 +1,39 @@
+#include "HeavyGun.h"
+
+#include <GameEngineCore/GameEngineRender.h>
+#include <GameEnginePlatform/GameEngineInput.h>
+#include <GameEngineBase/GameEngineMath.h>
+#include <GameEngineCore/GameEngineCollision.h>
+#include "Player.h"
+#include "ContentsEnums.h"
+
+
+HeavyGun::HeavyGun()
+{
+
+
+}
+
+HeavyGun::~HeavyGun()
+{
+
+
+}
+
+void HeavyGun::Start()
+{
+
+
+}
+
+void HeavyGun::Update(float _DeltaTime)
+{
+
+
+}
+
+void HeavyGun::Render(float _Time)
+{
+
+
+}
---
+++ GameEngineContents/HeavyGun.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <GameEngineCore/GameEngineActor.h>
+#include "ContentsEnums.h"
+class HeavyGun : public GameEngineActor
+{
+
+
+
+public:
+
+
+	//enum Direction Dir = Direction::IDLE;
+	// constrcuter destructer
+	HeavyGun();
+	~HeavyGun();
+	Direction Dir = Direction::IDLE;
+	// delete Function
+	HeavyGun(const HeavyGun& _Other) = delete;
+	HeavyGun(HeavyGun&& _Other) noexcept = delete;
+	HeavyGun& operator=(const HeavyGun& _Other) = delete;
+	HeavyGun& operator=(HeavyGun&& _Other) noexcept = delete;
+	float4 MoveDir = float4::Zero;
+	bool test = false;
+	GameEngineRender* AnimationRender = nullptr;
+	GameEngineCollision* Collision = nullptr;
+	float4 CurPos = float4::Zero;
+
+protected:
+	void Start() override;
+	void Update(float _DeltaTime) override;
+	void Render(float _Time) override;
+private:
+
+
+};
+
+
